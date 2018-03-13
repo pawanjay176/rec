3d43c05b5760e72662a2147de199d948c3fbbc3d
pawanjay176 <pawandhananjay@gmail.com>
2018-03-13 22:30:20 +0530

    added migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const GreenToken = artifacts.require('./GreenToken')
+const Exchange = artifacts.require('./Exchange')
 
 module.exports = function(deployer) {
     deployer.deploy(GreenToken);
+    deployer.deploy(Exchange);
 }
